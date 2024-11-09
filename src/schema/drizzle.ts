0fb203a274a6fd5f95bf2cf376f2704b2abf947a
SandroMaglione <lass.maglio@gmail.com>
2024-11-09 16:51:41 +0100

    initial db schema

--- src/schema/drizzle.ts
+++ src/schema/drizzle.ts
@@ -1,8 +1,29 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { date, integer, pgEnum, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+export const foodTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+  brand: varchar({ length: 255 }),
+  calories: integer().notNull(),
+  fat: integer().notNull(),
+  fatSaturated: integer().notNull().default(0),
+  salt: integer().notNull().default(0),
+  carbohydrates: integer().notNull(),
+  fibers: integer().notNull().default(0),
+  sugars: integer().notNull().default(0),
+  proteins: integer().notNull(),
+});
+
+export const mealEnum = pgEnum("meal", [
+  "breakfast",
+  "lunch",
+  "dinner",
+  "snacks",
+]);
+
+export const servingTable = pgTable("users", {
+  date: date().primaryKey(),
+  meal: mealEnum().notNull(),
+  foodId: integer("food_id").references(() => foodTable.id),
+  quantity: integer().notNull(),
 });
