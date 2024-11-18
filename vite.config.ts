1f093ffd3e970542651a880f9c206d3eea6764dd
SandroMaglione <lass.maglio@gmail.com>
2024-11-18 15:37:48 +0100

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,42 @@ import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ["./src/drizzle/*.sql"],
-  plugins: [tsconfigPaths(), tailwindcss(), TanStackRouterVite({}), react()],
+  plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
+    TanStackRouterVite({}),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        maximumFileSizeToCacheInBytes: 15_000_000,
+      },
+      manifest: {
+        name: "Local Calories Tracker",
+        short_name: "LocalCalTrack",
+        description: "Keep track of your diet, locally on your device",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: ["@electric-sql/pglite"],
   },
